8b8e2491e11c6c532f441edfe4dec27cb49cf61d
Sweta Adhikari <120324683+Swetaadhikari01@users.noreply.github.com>
2026-01-18 22:43:58 +0530

    Toggle CORS middleware configuration
    Comment out the CORS middleware configuration.

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,17 +9,19 @@ const cors = require('cors');
 
 const app = express();
 
-/*
+
 app.use(cors({
   origin: process.env.CLIENT_URL ||"http://localhost:5173",
   credentials: true
 }));
-*/
 
+
+/* 
 app.use(cors({
   origin: "http://localhost:5173",
   credentials: true
 }));
+*/
 
 
 app.use(cookieParser());
