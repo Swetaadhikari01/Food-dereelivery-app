c9bd89b057167fa483c355dfe1dcf56d311c0932
Sweta Adhikari <120324683+Swetaadhikari01@users.noreply.github.com>
2026-01-19 13:23:43 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,14 +9,24 @@ const cors = require('cors');
 
 const app = express();
 
+const allowedOrigin = "https://food-dereelivery-frontend.onrender.com";
 
-app.use(cors({
+app.use(
+  cors({
+    origin: allowedOrigin,
+    credentials: true,
+  })
+);
+
+// Handle preflight requests
+app.options("*", cors());
+/*app.use(cors({
   origin: process.env.CLIENT_URL||"http://localhost:5173",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
 }));
-
+*/
 
 /* 
 app.use(cors({
