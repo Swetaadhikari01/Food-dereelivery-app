0abeb27066b3c8bab1993985bf94cc3a451be2db
Sweta Adhikari <120324683+Swetaadhikari01@users.noreply.github.com>
2026-01-19 11:22:36 +0530

    Enhance cookie settings for JWT token

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -26,7 +26,13 @@ async function registerUser(req, res) {
     id: user._id,
   }, process.env.JWT_SECRET)
 
-  res.cookie("token", token, { httpOnly: true });
+ res.cookie("token", token, {
+  httpOnly: true,
+  secure: true,          // REQUIRED for HTTPS
+  sameSite: "none",      // REQUIRED for cross-domain
+  maxAge: 7 * 24 * 60 * 60 * 1000,
+});
+
 
 
   res.status(201).json({
